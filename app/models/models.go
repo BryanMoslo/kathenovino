28ce5e17702dc5a708148833a27711e85501798e
balgutria <balgutria@motus.com>
2023-03-18 15:09:19 -0500

    vamo a ve

--- app/models/models.go
+++ app/models/models.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"bytes"
+	"kathenovino"
 	"kathenovino/config"
+	"log"
 
 	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/pop/v6"
@@ -13,9 +16,14 @@ var (
 	db = buffalotools.DatabaseProvider(config.FS())
 )
 
-// DB returns the DB connection for the current environment.
-func DB() *pop.Connection {
-	return db(envy.Get("GO_ENV", "development"))
+var env = envy.Get("GO_ENV", "development")
+
+// DB is a connection to your database to be used
+// throughout your application.
+var DB *pop.Connection
+
+func init() {
+	initDB()
 }
 
 // FindDB allows to pull a connection by the name of it
@@ -23,4 +31,23 @@ func DB() *pop.Connection {
 // from.
 func FindDB(connname string) *pop.Connection {
 	return db(connname)
+}
+
+func initDB() {
+	bf, err := kathenovino.Config.Find("database.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pop.LoadFrom(bytes.NewReader(bf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB, err = pop.Connect(env)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pop.Debug = env == "development"
 }
